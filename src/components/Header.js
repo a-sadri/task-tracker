0d96d13b632f3772d20ffcdea3d441a542940b62
Ali <asadri721@gmail.com>
2021-06-26 21:09:52 +0430

    onClick event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import Button from "./Button"
 
 const Header = ({ title }) => {
+
+    const onClick = (e) => {
+        console.log(e)
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color="green" text="Add" />
+            <Button onClick={onClick} color="green" text="Add" />
         </header>
     )
 }
